820b15b480953b3dc193eb4685ba0e78be1dd98b
Mukul Dharwadkar <mukul.dharwadkar@gmail.com>
2025-05-01 19:45:08 -0700

    Changed network settings to work on network

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,8 @@ const session = require('express-session');
 const cors = require('cors');
 const passport = require('./auth/passport');
 const authRoutes = require('./routes/auth');
-const profileRoutes = require('./routes/profiles');
+const profileRoutes = require('./routes/profile');
+const port = 5000;
 
 const app = express();
 
@@ -13,7 +14,16 @@ app.use(session({ secret: 'your-secret', resave: false, saveUninitialized: false
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.get('/', (req, res) => {
+    res.json({ message: 'Aphians Server Running' });
+  });
+
 app.use('/auth', authRoutes);
 app.use('/api/profile', profileRoutes);
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+// app.listen(5000, () => console.log('Server running on port 5000'));
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Aphians Server running on port ${port}`);
+  }).on('error', (err) => {
+    console.error('Server startup error:', err);
+  });
